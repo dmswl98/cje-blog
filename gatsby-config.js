6b64683337d4e6cace4fb2b221a5525cb7491db2
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-02-03 00:50:47 +0900

    chore: formatter, tsconfig 설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,11 +10,18 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `cje의 개발 일기`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-typescript',
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
